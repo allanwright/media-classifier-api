d3d27429fa759a9d09919bc3f09e56ad69e221c3
Allan Wright <allan.s.wright@gmail.com>
2019-10-03 09:36:40 +0000

    Initial commit of working version

---
+++ classify/__init__.py
@@ -0,0 +1,25 @@
+import logging
+import azure.functions as func
+from mccore import Classifier
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('classify function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        classifier = Classifier.load_default()
+        label, estimate = classifier.predict(name)
+        return func.HttpResponse(f"Predicted {label} with {estimate * 100}% confidence.")
+    else:
+        return func.HttpResponse(
+             "Please pass a name on the query string or in the request body",
+             status_code=400
+        )
