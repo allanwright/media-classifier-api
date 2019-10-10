ad128a3572c8b44c0b29b32f615d29fc6ab8d1aa
Allan Wright <allan.s.wright@gmail.com>
2019-10-10 09:59:12 +0000

    GraphQL function initial commit

---
+++ gql/__init__.py
@@ -0,0 +1,33 @@
+import json
+import logging
+import azure.functions as func
+from graphene import Schema
+import lib.schema
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Executing GraphQL function.')
+
+    try:
+        query = req.get_body().decode()
+    except ValueError:
+        pass
+
+    if query:
+        schema = Schema(lib.schema.Query)
+        results = schema.execute(query)
+
+        if results.errors:
+            # TODO: Return errors in the proper format
+            # https://graphql.org/learn/serving-over-http/#response
+            return func.HttpResponse(
+                results.errors[0].args[0],
+                status_code=400)
+        else:
+            return func.HttpResponse(
+                json.dumps(results.data),
+                mimetype='application/json')
+    else:
+        return func.HttpResponse(
+             "Please pass a GraphQL query in the request body.",
+             status_code=400
+        )
