5b7c2e7218d353491182c1ac10188d88a09b1b18
Allan Wright <allan.s.wright@gmail.com>
2019-10-29 10:13:48 +0000

    Add http response helper module

--- gql/__init__.py
+++ gql/__init__.py
@@ -2,6 +2,7 @@ import json
 import logging
 import azure.functions as func
 from graphene import Schema
+from .helpers import responses
 from .schema.Query import Query
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -15,19 +16,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     if query:
         schema = Schema(Query)
         results = schema.execute(query)
-
-        if results.errors:
-            # TODO: Return errors in the proper format
-            # https://graphql.org/learn/serving-over-http/#response
-            return func.HttpResponse(
-                results.errors[0].args[0],
-                status_code=400)
-        else:
-            return func.HttpResponse(
-                json.dumps(results.data),
-                mimetype='application/json')
+        return responses.graphql(results)
     else:
-        return func.HttpResponse(
-             "Please pass a GraphQL query in the request body.",
-             status_code=400
-        )
+        return responses.bad_request(
+            'Please pass a GraphQL query in the request body.')
